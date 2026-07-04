ca410cf42a424bc61a795c3b92fe0d046eb4080f
Amine <amine.benelbar@gmail.com>
2026-07-04 17:47:43 -0500

    Added the dependencies to get the test case to work and finished the first test case

---
+++ src/test/java/com/demo/FirstRestAssuredProjectTests/RestAssuredUsersTest.java
@@ -0,0 +1,31 @@
+package com.demo.FirstRestAssuredProjectTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+class RestAssuredUsersTest {
+
+	@Test
+	void getUsersTest() {
+		
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		RequestSpecification req = RestAssured.given();
+		Response res = req.when().get("/users");
+		String firstName = res.jsonPath().getString("[0].name");
+		String firstUsername = res.jsonPath().getString("[0].username");
+		int firstId = res.jsonPath().getInt("[0].id");
+		System.out.println("First User Name: " + firstName);
+		System.out.println("First User Username: " + firstUsername);
+		System.out.println("First User ID: " + firstId);
+	    assertEquals(200, res.statusCode());
+	    assertEquals("Leanne Graham", firstName);
+	    assertEquals("Bret", firstUsername);
+	    assertEquals(1, firstId); 
+
+	} 
+
+}
+
