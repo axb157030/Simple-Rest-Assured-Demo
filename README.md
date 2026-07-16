4af0cba619fc9e759439d1fc9d4ca2727a1abaff
Amine <amine.benelbar@gmail.com>
2026-07-15 21:28:29 -0500

    Used Allure to show the unit test using a UI and made a Docker Compose YAML file to do that as well

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # Simple-Rest-Assured-Demo
 
-Demo to make and test Rest Assured Test cases
+Demo to make and test Rest Assured Test cases and to build and run it with Docker.
+Also a demo to visualize test cases with [Allure Report](https://allurereport.org/docs/)
+
+---
+
+## Getting the test report represented with a UI with Docker
+
+This project uses Docker Compose to automate API testing with Maven and publish Allure reports using the Allure Docker Service.  
+The setup contains three services:
+
+- **tests** — runs Maven tests inside a container and uploads Allure results  
+- **allure-service** — receives results and generates Allure reports  
+- **allure-ui** — displays the generated reports in a browser  
+
+Shared volumes allow test results and reports to move between containers.
+
+---
+
+## Build and run the Docker container
+
+
+```bash
+docker-compose up --build
+```
+
+Your application will now be available at:
+
+[http://localhost:5252](http://localhost:5252)
+
+---
+
+## Getting the test report represented with a UI when running the application locally.
+
+### Run unit tests
+```bash
+mvn clean test .
+```
+- This mvn test command also runs the test case and shows also the printed statements in the test case in 
+the terminal
+
+### Make the Allure Report UI
+```bash
+mvn allure:report
+```
+### Serve the Allure reports
+```bash
+mvn allure:serve
+```
+- Allure Report will display on a random port but in the terminal the URL where it should show up
+
+
+
+
--- src/test/java/com/demo/FirstRestAssuredProjectTests/RestAssuredUsersTest.java
+++ src/test/java/com/demo/FirstRestAssuredProjectTests/RestAssuredUsersTest.java
@@ -2,32 +2,55 @@ package com.demo.FirstRestAssuredProjectTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-class RestAssuredUsersTest {
+public class RestAssuredUsersTest {
 
+	@Epic("API Tests")
+	@Feature("Users API")
+	@Story("Play with RestAssured with API that gives a set of users or a specific user")
+	@Description("Verify GET /users/1 returns status 200")
+	@AllureId("1")
 	@Test
-	void getUsersTest() {
-		
+	public void getUsersTest() {
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		Allure.step("Calling /users endpoint and getting first element of it");
 		RequestSpecification req = RestAssured.given();
-		Response res = req.when().get("/users");
+		Response res = req.filters(new AllureRestAssured()).when().get("/users");
+		String firstUserJson = res.jsonPath().getMap("[0]").toString();
 		String firstName = res.jsonPath().getString("[0].name");
 		String firstUsername = res.jsonPath().getString("[0].username");
 		int firstId = res.jsonPath().getInt("[0].id");
+		Allure.addAttachment("First User Object", "application/json", firstUserJson);
 		System.out.println("First User Name: " + firstName);
 		System.out.println("First User Username: " + firstUsername);
 		System.out.println("First User ID: " + firstId);
 		for(Header head: res.headers()) {
 			System.out.println("header: " + head.toString());
 		}
-	    assertEquals(200, res.statusCode());
-	    assertEquals("Leanne Graham", firstName);
-	    assertEquals("Bret", firstUsername);
-	    assertEquals(1, firstId); 
+	    Allure.step("Validating status code which should be 200", () -> {
+	        assertEquals(200, res.statusCode());
+	    });
+	    Allure.step("Validating first user name", () -> {
+	        assertEquals("Leanne Graham", firstName);
+	    });
+	    Allure.step("Validating first user username", () -> {
+	        assertEquals("Bret", firstUsername);
+	    });
+	    Allure.step("Validating first user ID, which should be 1", () -> {
+	        assertEquals(1, firstId);
+	    });
 
 	} 
 
