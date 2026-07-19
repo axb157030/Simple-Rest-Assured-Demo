8d4c67469c4eacfa3bbe016b4afb7879ab78df27
Amine <amine.benelbar@gmail.com>
2026-07-18 21:57:47 -0500

    Add another Rest Assured unit test and modified Docker compose YAML file to include attachments in the report from Docker container url from the application

--- src/test/java/com/demo/FirstRestAssuredProjectTests/RestAssuredUsersTest.java
+++ src/test/java/com/demo/FirstRestAssuredProjectTests/RestAssuredUsersTest.java
@@ -1,6 +1,10 @@
 package com.demo.FirstRestAssuredProjectTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.qameta.allure.Allure;
@@ -16,18 +20,30 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class RestAssuredUsersTest {
+	
+	private RequestSpecification req;
+	
+	@BeforeAll
+	static void initBaseUrl() {
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		RestAssured.filters(new AllureRestAssured());
+	}
+		@BeforeEach
+		void init() {
+		req=RestAssured.given();
+	}
 
-	@Epic("API Tests")
+
+
+	@Epic("API Users Tests")
 	@Feature("Users API")
 	@Story("Play with RestAssured with API that gives a set of users or a specific user")
 	@Description("Verify GET /users/1 returns status 200")
 	@AllureId("1")
 	@Test
-	public void getUsersTest() {
-		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+	public void getFirstUsersTest() {
 		Allure.step("Calling /users endpoint and getting first element of it");
-		RequestSpecification req = RestAssured.given();
-		Response res = req.filters(new AllureRestAssured()).when().get("/users");
+		Response res = req.when().get("/users");
 		String firstUserJson = res.jsonPath().getMap("[0]").toString();
 		String firstName = res.jsonPath().getString("[0].name");
 		String firstUsername = res.jsonPath().getString("[0].username");
@@ -53,6 +69,37 @@ public class RestAssuredUsersTest {
 	    });
 
 	} 
+	
+	@Epic("Comment API Test")
+	@Feature("Getting first post from Comments API")
+	@Story("Play with RestAssured with API that gives a set of comments and testing response after query for first post on comments list from API")
+	@Description("Verify GET /comments and /comments?postId=1 returns status 200 and"
+			+ "that response of /comments?postId=1 has less elements than response of /comments")
+	@AllureId("2")
+	@Test
+	public void getFirstPostTest() {
+		req.basePath("/comments");
+		Response resComments = req.get();
+		Response resFirstPost = req.queryParam("postId", 1).get();
+		// BUFFER BEFORE AllureRestAssured touches the stream
+		String allCommentsJson = resComments.getBody().asString();
+		String firstPostJson = resFirstPost.getBody().asString();
+		// Attach buffered JSON
+		Allure.addAttachment("All Comments", "application/json", allCommentsJson);
+		Allure.addAttachment("First Post", "application/json", firstPostJson);
+		Allure.step("Validating status code to get comments should be 200", () -> {
+	        assertEquals(200, resComments.statusCode());
+	    });
+	    Allure.step("Validating status code to get first spost 200", () -> {
+	        assertEquals(200, resComments.statusCode());
+	    });
+	    
+	    Allure.step("Validating that number of elements directly in first post is less"
+	    		+ " then the number of elements directly in all the comments", () -> {
+	        assertTrue(resComments.jsonPath().getList("$").size() > resFirstPost.jsonPath().getList("$").size());
+	    });
+		
+	}
 
 }
 
